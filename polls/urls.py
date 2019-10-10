3d4bc93e7b1dd24d6c1844b33b74133a306437f2
グエン ミン ホアン <900016@ad.zenjukyo.gr.jp>
2019-10-10 17:39:27 +0900

    10-10

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import url
 from . import views
+app_name = "polls"
 urlpatterns = [
     path('hoang/', views.index),
-    url(r'3', views.ham),
+    path(r'3', views.ham),
+    re_path(r'^(?P<question_id>[1-9]+)/$',views.vote,name='vote'),
     url(r'^(?P<question_id>[1-9]+)/detail/$',views.detail),
     url(r'detail/',views.detail),
     url(r'1/',views.index1),
-    url(r'2/',views.index1)
+    url(r'2/',views.index2)
+    
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from .models import question, choice
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello Word")
+    
 def ham(request):
     return HttpResponse("<h1>Khó VL</h1><p> Cố thôi chứ biết làm sao</p>")
+
 def detail(request, question_id):
     return HttpResponse("Question text la: %s" % question_id)
+
 def index1(request):
     latest_question_list = question.objects.order_by('-time_pub')[:5]
     template = loader.get_template('polls/index.html')
@@ -17,7 +20,35 @@ def index1(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+
 def index2(request):
-    latest_question_list = question.objects.order_by('-pub_date')[:5]
-    output =join([q.question_text for q in latest_question_list])
+    try:
+        latest_question_list = question.objects.order_by('-time_pub')[:5]
+        output =",".join([q.question_text for q in latest_question_list])
+    except question.DoesNotExist:
+        raise Http404("Đã xảy ra lỗi")   
     return HttpResponse(output)
+
+def index3(request, question_id):
+    try:
+        latest_question_list = question.objects.get(pk=question_id)
+        output = {
+            latest_question_list: latest_question_list
+        }
+    except question.DoesNotExist:
+        raise Http404("Đã xảy ra lỗi nghiêm trọng")   
+    return render(request,"polls/index1.html",output)
+
+def vote(request, question_id):
+    question1 = get_object_or_404(question, pk=question_id)
+    try:
+        selected_choice = question1.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question1,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
