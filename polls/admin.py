87ccca3f9ab4c182e18e1a0a6f7ecf7ad5ecad67
グエン ミン ホアン <900016@ad.zenjukyo.gr.jp>
2019-10-16 17:52:36 +0900

    16-10

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import question, choice
+from .models import question, choice, Blog, Author, Entry
 admin.site.register(question)
 admin.site.register(choice)
+admin.site.register(Blog)
+admin.site.register(Author)
+admin.site.register(Entry)
