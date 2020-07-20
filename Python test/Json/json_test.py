bd95c33ad8a87bdb80f7639d174c0131ec1a7455
グエン ミン ホアン <900016@ad.zenjukyo.gr.jp>
2020-07-20 19:08:09 +0900

    1

--- Python test/Json/json_test.py
+++ Python test/Json/json_test.py
@@ -3,6 +3,6 @@ import os
 x = {'name': 'Hoang dai ca', 'age' : 32, 'male' : 'male'}
 with open (os.path.dirname(__file__)+"/file_json.json", "w+") as j:
     json.dump(x,j,indent=4)
-with open (os.path.dirname(__file__)+"/file_json.json", "a+") as i:
-    d = json.load(i.read())
+with open (os.path.dirname(__file__)+"/file_json.json", encoding='utf-8') as i:
+    d = json.load(i)
 print(d)
